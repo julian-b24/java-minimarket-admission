a054a07fd5526f783fa344fca5c67f772d49a3fa
Julian Bolanos <julian.camilob24@gmail.com>
2021-04-10 02:54:35 -0500

    Class Diagram added to docs. Project finished.

--- test/model/MinimarketTest.java
+++ test/model/MinimarketTest.java
@@ -21,18 +21,6 @@ class MinimarketTest {
 		market = new Minimarket(LocalDate.parse("2021-02-15"));
 	}
 	
-	public void setupScenary3() {
-		market = new Minimarket(LocalDate.parse("2021-02-06"));
-		market.getPersons().add(new Person("4783297", "CC"));
-		market.setNumAttemps(4);
-	}
-	
-	public void setupScenary4() {
-		market = new Minimarket(LocalDate.parse("2021-02-07"));
-		market.getPersons().add(new Person("234443", "CC"));
-		market.getPersons().add(new Person("1919121", "PP"));
-	}
-	
 	@Test
 	public void testAdmissionEvenDayAdmitted() {
 		
